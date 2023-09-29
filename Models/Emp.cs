bd7b0daba1ae3737ad02913cc6016352b8d0b1cc
Deepak PG <deepakpg121@gmail.com>
2023-09-29 19:55:03 +0530

    Create properties for database table values

---
+++ Models/Emp.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mvc.Models
+{
+    [Table("mvcRegistration")]
+    public class Emp
+    {
+        [Key]
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string gender { get; set; }
+        public string address { get; set; }
+        public string course { get; set; }
+        public string language { get; set; }
+
+    }
+}
